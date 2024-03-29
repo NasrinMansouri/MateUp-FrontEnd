e88c0348b9cfa3f44420219d3a0dae66185246e0
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-03-29 15:40:27 +0100

    first commit

---
+++ Front_End/App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [isLoaded] = useFonts({
+    "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
+    "nunitoSans-bold": require("./assets/fonts/NunitoSans7pt-Bold.ttf"),
+    "nunitoSans-regular": require("./assets/fonts/NunitoSans7pt-Regular.ttf"),
+    "nunitoSans-extraBold": require("./assets/fonts/NunitoSans7pt-ExtraBold.ttf"),
+  });
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync(); //hide the splashscreen
+    }
+  }, [isLoaded]);
+  if (!isLoaded) {
+    return null;
+  }
+  return (
+    <View style={styles.container} onLayout={handleOnLayout}>
+      <Text
+        style={{
+          fontFamily: "montserrat-black",
+          fontSize: 40,
+        }}
+      >
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
