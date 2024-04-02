60a73307f99e3cf54ba94fcf3af188d78ad3d6f2
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-02 07:53:32 +0200

    adds color system

---
+++ Front_End/app/config/colors.js
@@ -0,0 +1,11 @@
+export default {
+  orangePrimary: "#FF8047",
+  orangeSecondary: "#FE7000",
+  green: "#709872",
+  white: "#FFFEFB",
+  black: "#333333",
+  blackBc: "#252525",
+  gray: "#7A7878",
+  grayLight100: "#7A7878",
+  grayLight50: "#D6D0CA",
+};
