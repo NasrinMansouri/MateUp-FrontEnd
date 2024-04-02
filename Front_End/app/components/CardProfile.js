bee4014a5c2eb45e20dada24451d95bf3d86315f
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-02 08:25:09 +0200

    creates card with image and text

---
+++ Front_End/app/components/CardProfile.js
@@ -0,0 +1,108 @@
+// profile image with text
+
+//can be horizontal or vertical
+//image size can be changed
+//can have border
+//image size can be changed
+
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+import colors from "../config/colors";
+
+function CardProfile({
+  image,
+  name,
+  imageWidth,
+  imageHeight,
+  borderRadius,
+  cardWidth,
+  flexDirection,
+  cardRadius,
+  cardBorderColor,
+  cardBorderWidth,
+  fontFamily,
+  fontSize,
+  fontWeight,
+  onPress,
+}) {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View
+        style={[
+          styles.card,
+          {
+            width: cardWidth,
+            flexDirection: flexDirection,
+            borderWidth: cardBorderWidth,
+            borderColor: cardBorderColor,
+            borderRadius: cardRadius,
+          },
+        ]}
+      >
+        <Image
+          source={image}
+          style={[
+            styles.image,
+            {
+              width: imageWidth,
+              height: imageHeight,
+              borderRadius: borderRadius,
+            },
+          ]}
+        />
+
+        <Text
+          style={[
+            styles.text,
+            {
+              fontFamily: fontFamily,
+              fontSize: fontSize,
+              fontWeight: fontWeight,
+            },
+          ]}
+        >
+          {name}
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: colors.blackBc,
+    alignItems: "center",
+    marginRight: 4,
+  },
+  image: {
+    margin: 10,
+  },
+  text: {
+    color: colors.white,
+    marginBottom: 10,
+  },
+});
+
+export default CardProfile;
+
+//to be used in screen as:
+{
+  /* <CardProfile
+onPress={() => console.log("tapped")}
+flexDirection={"column"}
+cardWidth={97}
+image={require("./assets/person-1.jpg")}
+imageHeight={77}
+imageWidth={77}
+borderRadius={77 / 2}
+name="Jane Doe"
+fontSize={14}
+/> */
+}
