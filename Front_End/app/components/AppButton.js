0877eda04dda9d033ab52e8f1a2f17944f67d07b
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-02 08:02:54 +0200

    creates app button, possibility with icon

---
+++ Front_End/app/components/AppButton.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, Image } from "react-native";
+
+import colors from "../config/colors";
+
+function AppButton({ title, image, height = 45, width = "100%", onPress }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { height: height, width: width }]}
+      activeOpacity={0.8}
+      onPress={onPress}
+    >
+      {image && <Image source={image} style={styles.image} />}
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.green,
+    flexDirection: "row",
+    // width: "100%",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: colors.white,
+    textTransform: "uppercase",
+    fontFamily: "montserrat-black",
+  },
+  image: {
+    marginRight: 10,
+  },
+});
+
+export default AppButton;
+
+//to be used in screen as:
+//here using 2 buttons horizontally
+{
+  /* <View
+style={{
+  flexDirection: "row",
+  justifyContent: "space-between",
+  margin: 16,
+}}
+>
+<AppButton
+  title="join"
+  image={require("./assets/calendar.png")}
+  width={168}
+  height={32}
+  onPress={() => console.log("tapped")}
+/>
+<AppButton
+  title="join"
+  width={168}
+  height={32}
+  onPress={() => console.log("tapped")}
+/>
+</View> */
+}
