36925d2e9ad5b50ff45edb6807803dd74b248ec9
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-02 10:57:17 +0200

    creates bullet point with text component

---
+++ Front_End/app/components/BulletPointWithText.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const BulletPointWithText = ({
+  bulletColor,
+  width,
+  height,
+  marginRight,
+  fontFamily,
+  textColor,
+  text,
+  fontSize,
+  textTransform,
+}) => {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View
+        style={{
+          width: width,
+          height: height,
+          borderRadius: width / 2,
+          backgroundColor: bulletColor,
+          marginRight: marginRight,
+        }}
+      />
+      <Text
+        style={{
+          fontSize: fontSize,
+          textTransform: textTransform,
+          color: textColor,
+          fontFamily: fontFamily,
+        }}
+        numberOfLines={1}
+      >
+        {text}
+      </Text>
+    </View>
+  );
+};
+
+export default BulletPointWithText;
+
+// //to be used in screen as:
+
+// <BulletPointWithText
+// bulletColor={colors.orangePrimary}
+// width={5}
+// height={5}
+// borderRadius={5 / 2}
+// marginRight={4}
+// text="spinning"
+// textColor={colors.white}
+// fontFamily="nunitoSans-bold"
+// textTransform={"uppercase"}
+// fontSize={16}
+// />
