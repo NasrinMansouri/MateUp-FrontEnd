a4a94f8bae51f8fac8d23d40188dd4162c4daa43
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-02 08:02:54 +0200

    creates app button, possibility with icon

--- Front_End/App.js
+++ Front_End/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
+
+//for fonts
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from "react";
 SplashScreen.preventAutoHideAsync();
 
+import colors from "./app/config/colors";
+import AppButton from "./app/components/AppButton";
+
 export default function App() {
+  //for fonts
   const [isLoaded] = useFonts({
     "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
     "nunitoSans-bold": require("./assets/fonts/NunitoSans7pt-Bold.ttf"),
@@ -21,15 +27,26 @@ export default function App() {
     return null;
   }
   return (
-    <View style={styles.container} onLayout={handleOnLayout}>
+    <View
+      style={styles.container}
+      onLayout={handleOnLayout} //for fonts
+    >
       <Text
         style={{
           fontFamily: "montserrat-black",
           fontSize: 40,
+          color: colors.orangePrimary,
         }}
       >
         Open up App.js to start working on your app!
       </Text>
+
+      <AppButton
+        title="join"
+        width={168}
+        height={32}
+        onPress={() => console.log("tapped")}
+      />
 
       <StatusBar style="auto" />
     </View>
