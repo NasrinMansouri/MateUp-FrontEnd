a1f19768ddce39adb9be9e2e0deb7d82000ade4b
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-03 12:06:35 +0200

    creates a gallery component to display users club  members

--- Front_End/app/components/buddy/GalleryMatchClubMembers.js
+++ Front_End/app/components/buddy/GalleryMatchClubMembers.js
@@ -1,12 +1,56 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React from "react";
+import CardMatchClubMembers from "./CardMatchClubMembers";
 
-export default function GalleryMatchClubMembers() {
+export default function GalleryMatchClubMembers({ meetClubMembers }) {
   return (
-    <View>
-      <Text>GalleryMeetYourClubMembers</Text>
-    </View>
+    <FlatList
+      style={styles.container}
+      horizontal
+      data={meetClubMembers}
+      keyExtractor={(meetClubMembers) => meetClubMembers.id.toString()}
+      renderItem={({ item }) => (
+        <CardMatchClubMembers
+          name={item.name}
+          image={item.image}
+          titles={item.titles}
+        />
+      )}
+    />
   );
 }
 
 const styles = StyleSheet.create({});
+
+//to be use in screen as:
+{
+  /* <GalleryMatchClubMembers meetClubMembers={meetClubMembersData} /> */
+}
+
+//dummy data for testing
+// const meetClubMembersData = [
+//   {
+//     id: 1,
+//     name: "John Doeeeeeeeeeeeeeeeeee",
+//     image: require("./assets/person-1.jpg"),
+//     titles: ["strength training", "running"],
+//   },
+//   {
+//     id: 2,
+//     name: "ray pather ",
+//     image: require("./assets/person-1.jpg"),
+//     titles: ["strength training", "running", "swimming", "yoga", "boxing"],
+//   },
+//   {
+//     id: 3,
+//     name: "ray pather ",
+//     image: require("./assets/person-1.jpg"),
+//     titles: ["strength training", "running", "swimming", "yoga", "boxing"],
+//   },
+//   {
+//     id: 3,
+//     name: "ray pather ",
+//     image: require("./assets/person-1.jpg"),
+//     titles: ["strength training"],
+//   },
+// ];
