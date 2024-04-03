6ff52af4fdb370d1ec45bee272af859537c47c6b
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-03 11:43:45 +0200

    changes file name

---
+++ Front_End/app/components/buddy/GalleryMeetYourClubMembers.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function GalleryMeetYourClubMembers() {
+  return (
+    <View>
+      <Text>GalleryMeetYourClubMembers</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
