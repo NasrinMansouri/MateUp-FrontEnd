3f9d6b0f0567844d1558aa1982ff57b98586960c
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-03 12:48:09 +0200

    adds a touchable opacity to card

--- Front_End/app/components/buddy/CardMatchClubMembers.js
+++ Front_End/app/components/buddy/CardMatchClubMembers.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 
 import CardProfile from "../CardProfile";
 import colors from "../../config/colors";
 import BulletPointWithText from "../BulletPointWithText";
 
-export default function CardMatchClubMembers({ name, image, titles }) {
+export default function CardMatchClubMembers({ name, image, titles, onPress }) {
   // Maximum number of workout types to display
   const maxWorkoutTypes = 2;
 
@@ -19,62 +19,64 @@ export default function CardMatchClubMembers({ name, image, titles }) {
   const showThirdWorkout = titles.length > 2;
 
   return (
-    <View style={styles.container}>
-      <View style={styles.profile}>
-        <CardProfile
-          name={name}
-          image={image}
-          imageWidth={116}
-          imageHeight={116}
-          imageRadius={116 / 2}
-          cardWidth={142}
-          cardHeight={140}
-          backgroundColor={colors.blackBc}
-          flexDirection={"column"}
-          fontFamily={"nunitoSans-regular"}
-          fontSize={16}
-          textTransform={"capitalize"}
-          textColor={colors.white}
-        />
+    <TouchableOpacity onPress={onPress} activeOpacity={0.8}>
+      <View style={styles.container}>
+        <View style={styles.profile}>
+          <CardProfile
+            name={name}
+            image={image}
+            imageWidth={116}
+            imageHeight={116}
+            imageRadius={116 / 2}
+            cardWidth={142}
+            cardHeight={140}
+            backgroundColor={colors.blackBc}
+            flexDirection={"column"}
+            fontFamily={"nunitoSans-regular"}
+            fontSize={16}
+            textTransform={"capitalize"}
+            textColor={colors.white}
+          />
+        </View>
+        <View style={styles.bulletPoints}>
+          {displayedTitles.map((title, id) => (
+            <View key={id} style={styles.workoutContainer}>
+              <BulletPointWithText
+                bulletColor={colors.orangePrimary}
+                width={5}
+                height={5}
+                borderRadius={5 / 2}
+                marginRight={4}
+                marginBottom={4}
+                textColor={colors.white}
+                fontFamily="nunitoSans-extraBold"
+                textTransform={"uppercase"}
+                fontSize={12}
+                title={title}
+              />
+            </View>
+          ))}
+          {showThirdWorkout && (
+            <View style={styles.lastWorkoutContainer}>
+              <BulletPointWithText
+                bulletColor={colors.orangePrimary}
+                width={5}
+                height={5}
+                borderRadius={5 / 2}
+                marginRight={4}
+                marginBottom={4}
+                textColor={colors.white}
+                fontFamily="nunitoSans-extraBold"
+                textTransform={"uppercase"}
+                fontSize={12}
+                title={titles[maxWorkoutTypes]} // Display the third workout title
+              />
+              {showEllipsis && <Text style={styles.ellipsis}>more</Text>}
+            </View>
+          )}
+        </View>
       </View>
-      <View style={styles.bulletPoints}>
-        {displayedTitles.map((title, id) => (
-          <View key={id} style={styles.workoutContainer}>
-            <BulletPointWithText
-              bulletColor={colors.orangePrimary}
-              width={5}
-              height={5}
-              borderRadius={5 / 2}
-              marginRight={4}
-              marginBottom={4}
-              textColor={colors.white}
-              fontFamily="nunitoSans-extraBold"
-              textTransform={"uppercase"}
-              fontSize={12}
-              title={title}
-            />
-          </View>
-        ))}
-        {showThirdWorkout && (
-          <View style={styles.lastWorkoutContainer}>
-            <BulletPointWithText
-              bulletColor={colors.orangePrimary}
-              width={5}
-              height={5}
-              borderRadius={5 / 2}
-              marginRight={4}
-              marginBottom={4}
-              textColor={colors.white}
-              fontFamily="nunitoSans-extraBold"
-              textTransform={"uppercase"}
-              fontSize={12}
-              title={titles[maxWorkoutTypes]} // Display the third workout title
-            />
-            {showEllipsis && <Text style={styles.ellipsis}>more</Text>}
-          </View>
-        )}
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
@@ -82,11 +84,11 @@ const styles = StyleSheet.create({
   container: {
     width: 170,
     height: 220,
-    marginRight: 16,
+    marginRight: 8,
   },
   profile: {
     marginBottom: 18,
-    alignItems: "center",
+    // alignItems: "center",
   },
   bulletPoints: {
     flexDirection: "column", // Ensure workout types are displayed horizontally
