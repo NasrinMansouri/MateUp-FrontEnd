87a6b46007f4ba762fc71b76261f5856422603ac
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-03 19:12:14 +0200

    test

--- Front_End/App.js
+++ Front_End/App.js
@@ -10,8 +10,10 @@ SplashScreen.preventAutoHideAsync();
 import colors from "./app/config/colors";
 import AppButtonBorder from "./app/components/AppButtonBorder";
 
-import CardAllCoaches from "./app/components/coach/CardAllCoaches";
 import GalleryAllCoaches from "./app/components/coach/GalleryAllCoaches";
+import Avatar from "./app/components/coach/Avatar";
+import GroupAvatar from "./app/components/coach/Avatar";
+
 const coachesData = [
   {
     id: 1,
@@ -23,7 +25,7 @@ const coachesData = [
   {
     id: 2,
     name: "ray pather ",
-    image: require("./assets/person-1.jpg"),
+    image: require("./assets/person2.jpg"),
     location: "los angeles street" + " 123",
     titles: ["strength training", "running", "swimming", "yoga", "boxing"],
   },
@@ -44,6 +46,17 @@ const coachesData = [
 ];
 
 export default function App() {
+  const buddies = [
+    { id: 1, image: require("./assets/person-1.jpg") },
+    { id: 2, image: require("./assets/person2.jpg") },
+    { id: 3, image: require("./assets/person2.jpg") },
+    { id: 4, image: require("./assets/person2.jpg") },
+    { id: 5, image: require("./assets/person2.jpg") },
+    { id: 6, image: require("./assets/person2.jpg") },
+    { id: 7, image: require("./assets/person2.jpg") },
+    // Add more buddies as needed
+  ];
+
   //for fonts
   const [isLoaded] = useFonts({
     "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
@@ -64,6 +77,8 @@ export default function App() {
     <View style={styles.container} onLayout={handleOnLayout}>
       <ScrollView style={styles.scrollview}>
         <GalleryAllCoaches meetAllCoaches={coachesData} />
+        <GroupAvatar buddies={buddies} />
+        {/* <Avatar image={coachesData} /> */}
         <StatusBar style="auto" />
       </ScrollView>
       <View
@@ -89,8 +104,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.blackBc,
     paddingTop: 50,
-    // paddingLeft: 16,
-    // paddingRight: 16,
   },
   scrollview: {
     flex: 1,
