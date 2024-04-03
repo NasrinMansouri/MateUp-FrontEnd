22fec87cfc354950ee5f0c54efc0d091e15d6edd
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-03 16:01:57 +0200

    create component to display member with similar workout type

---
+++ Front_End/app/components/buddy/GalleryMatchBasedWorkout.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import CardMatchBasedWorkout from "./CardMatchBasedWorkout";
+import { FlatList } from "react-native";
+
+export default function GalleryMatchBasedWorkout({ matchMemberWorkout }) {
+  return (
+    <FlatList
+      style={styles.container}
+      horizontal
+      data={matchMemberWorkout}
+      keyExtractor={(matchMemberWorkout) => matchMemberWorkout.id.toString()}
+      renderItem={({ item }) => (
+        <CardMatchBasedWorkout
+          name={item.name}
+          image={item.image}
+          location={item.location}
+          titles={item.titles}
+        />
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({});
+
+//to be use in screen as:
+{
+  /* <GalleryMatchBasedWorkout matchMemberWorkout={matchClubMembersData} /> */
+}
+
+//dummy data for testing
+// const matchClubMembersData = [
+//     {
+//       id: 1,
+//       name: "John Doeeeeeeeeeeeeeeeeee",
+//       image: require("./assets/person-1.jpg"),
+//       location: "los angeles street" + " 123",
+//       titles: ["strength training", "running"],
+//     },
+//     {
+//       id: 2,
+//       name: "ray pather ",
+//       image: require("./assets/person-1.jpg"),
+//       location: "los angeles street" + " 123",
+//       titles: ["strength training", "running", "swimming", "yoga", "boxing"],
+//     },
+//     {
+//       id: 3,
+//       name: "ray pather ",
+//       image: require("./assets/person-1.jpg"),
+//       location: "los angeles street" + " 123",
+//       titles: ["strength training", "running", "swimming", "yoga", "boxing"],
+//     },
+//     {
+//       id: 4,
+//       name: "ray pather ",
+//       image: require("./assets/person-1.jpg"),
+//       location: "los angeles street" + " 123",
+//       titles: ["strength training"],
+//     },
+//   ];
