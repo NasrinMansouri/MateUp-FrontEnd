b04c4e965ebab2a744224a40b8fe6d305c382f2c
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-04 12:16:02 +0200

    test

--- Front_End/App.js
+++ Front_End/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
 import { useFonts } from "expo-font";
 
 //for fonts
@@ -10,40 +10,22 @@ SplashScreen.preventAutoHideAsync();
 import colors from "./app/config/colors";
 import AppButtonBorder from "./app/components/AppButtonBorder";
 
-import GalleryAllCoaches from "./app/components/coach/GalleryAllCoaches";
-import Avatar from "./app/components/coach/Avatar";
-import GroupAvatar from "./app/components/coach/Avatar";
+import GroupAvatar from "./app/components/challenge/GroupAvatar";
+import LocationWithIcon from "./app/components/LocationWithIcon";
+import CardChallengBuddiesJoined from "./app/components/challenge/CardChallengBuddiesJoined";
 
-const coachesData = [
-  {
-    id: 1,
-    name: "John Doee",
-    image: require("./assets/person-1.jpg"),
-    location: "los angeles street" + " 123",
-    titles: ["strength training", "running"],
-  },
-  {
-    id: 2,
-    name: "ray pather ",
-    image: require("./assets/person2.jpg"),
-    location: "los angeles street" + " 123",
-    titles: ["strength training", "running", "swimming", "yoga", "boxing"],
-  },
-  {
-    id: 3,
-    name: "aaron",
-    image: require("./assets/person-1.jpg"),
-    location: "los angeles street" + " 123",
-    titles: ["strength training", "running", "swimming", "yoga", "boxing"],
-  },
-  {
-    id: 4,
-    name: "ray pather ",
-    image: require("./assets/person-1.jpg"),
-    location: "los angeles street" + " 123",
-    titles: ["strength training"],
-  },
-];
+// const challengeData = [
+//   {
+//     image: require("./images/challenge.jpg"), // Example image path
+//     challengeName: "Weekly Challenge",
+//     duration: 7, // Example duration (in hours or days)
+//     beginingDate: "2024-04-01",
+//     endingDate: "2024-04-07",
+//     year: 2024,
+//     onPress: () => {}, // Example onPress function
+//     BuddiesImage: require("./images/buddies.jpg"),
+//   },
+// ];
 
 export default function App() {
   const buddies = [
@@ -74,10 +56,65 @@ export default function App() {
 
   return (
     <View style={styles.container} onLayout={handleOnLayout}>
-      <ScrollView style={styles.scrollview}>
-        <GalleryAllCoaches meetAllCoaches={coachesData} />
-        <GroupAvatar buddies={buddies} />
-        {/* <Avatar image={coachesData} /> */}
+      <ScrollView style={styles.View}>
+        {/* <CardChallengBuddiesJoined
+          image={require("./assets/person2.jpg")}
+          challengeName="Weekly Challenge"
+          duration={7} // Example duration (in hours or days)
+          beginingDate="2024-04-01"
+          endingDate="2024-04-07"
+          year={2024}
+          Buddies={[
+            require("./assets/person2.jpg"),
+            require("./assets/person-1.jpg"),
+            require("./assets/person2.jpg"),
+            require("./assets/person2.jpg"),
+          ]}
+        /> */}
+        <CardChallengBuddiesJoined
+          image={require("./assets/person2.jpg")}
+          challengeName="Weekly Challenge"
+          duration="7 Hours"
+          beginingDate="AUG 3"
+          endingDate="SEP 3"
+          year={2024}
+          borderWidth={0}
+          cardHeight={284}
+          cardWidth="100%"
+        />
+
+        <CardChallengBuddiesJoined
+          onPress={() => console.log("card pressed")}
+          image={require("./assets/person2.jpg")}
+          challengeName="Weekly Challenge"
+          duration="7 Hours"
+          beginingDate="AUG 3"
+          endingDate="SEP 3"
+          year={2024}
+          Buddies={[
+            { id: 1, image: require("./assets/person2.jpg") },
+            { id: 2, image: require("./assets/person-1.jpg") },
+            { id: 3, image: require("./assets/person2.jpg") },
+            { id: 4, image: require("./assets/person2.jpg") },
+          ]}
+        />
+
+        {/* {console.log("budiesImage", require("./assets/person2.jpg"))} */}
+        {/* <GroupAvatar buddies={buddies} /> */}
+        {/* <LocationWithIcon location={"los angeles street" + " 123"} /> */}
+        {/* <View style={{ flexDirection: "row" }}>
+          <Image source={require("./assets/line.png")} />
+          <Image
+            source={require("./assets/person2.jpg")}
+            style={{
+              width: 78,
+              height: 78,
+              borderRadius: 78 / 2,
+              position: "absolute",
+              left: 32,
+            }}
+          />
+        </View> */}
         <StatusBar style="auto" />
       </ScrollView>
       <View
@@ -104,7 +141,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.blackBc,
     paddingTop: 50,
   },
-  scrollview: {
+  View: {
     flex: 1,
   },
 });
