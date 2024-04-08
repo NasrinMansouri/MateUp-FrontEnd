584fd0a82d4a64efaa56d71047b033bba5134b29
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-08 16:11:08 +0200

    horizontal scroll indicator= false

--- Front_End/app/components/FontLoader.js
+++ Front_End/app/components/FontLoader.js
@@ -3,6 +3,8 @@ import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from "react";
 SplashScreen.preventAutoHideAsync();
 
+import { View } from "react-native";
+
 export default function FontLoader({ children }) {
   //for fonts
   const [isLoaded] = useFonts({
@@ -21,3 +23,28 @@ export default function FontLoader({ children }) {
   }
   return children;
 }
+// import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
+// import { useCallback } from "react";
+// SplashScreen.preventAutoHideAsync();
+
+// import { View } from "react-native";
+
+// export default function FontLoader({ children }) {
+//   //for fonts
+//   const [isLoaded] = useFonts({
+//     "montserrat-black": require("../../assets/fonts/Montserrat-Black.ttf"),
+//     "nunitoSans-bold": require("../../assets/fonts/NunitoSans7pt-Bold.ttf"),
+//     "nunitoSans-regular": require("../../assets/fonts/NunitoSans7pt-Regular.ttf"),
+//     "nunitoSans-extraBold": require("../../assets/fonts/NunitoSans7pt-ExtraBold.ttf"),
+//   });
+//   const handleOnLayout = useCallback(async () => {
+//     if (isLoaded) {
+//       await SplashScreen.hideAsync(); //hide the splashscreen
+//     }
+//   }, [isLoaded]);
+//   if (!isLoaded) {
+//     return null;
+//   }
+//   return <View onLayout={handleOnLayout}>{children}</View>;
+// }
--- Front_End/app/screens/home/MainHomeScreen.js
+++ Front_End/app/screens/home/MainHomeScreen.js
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React from "react";
 
 import Line from "../../components/Line";
@@ -13,47 +13,174 @@ import GalleryPeopleYouMightKnow from "../../components/home/GalleryPeopleYouMig
 import CardMeetTheMemberOfTheMonth from "../../components/home/CardMeetTheMemberOfTheMonth";
 import GalleryEducationalContent from "../../components/home/GalleryEducationalContent";
 
+// export default function MainHomeScreen() {
+//   return (
+//     <View>
+//       <ScrollView showsVerticalScrollIndicator={false}>
+//         <Screen style={styles.container}>
+//           <GalleryBuddies style={{ paddingLeft: 16, marginBottom: 40 }} />
+//           <Line marginBottom={40} />
+//           <Text style={styles.welcomText}>WELCOME REZA</Text>
+//           <UserNextWorkoutPlanning />
+//           <View style={styles.buddiesWorkoutContainer}>
+//             <Text style={styles.titleText}>upcoming buddies workout</Text>
+//             <Text style={styles.subTitleText}>
+//               Discover your buddies' latest workout routines, Join in for Fun
+//               and Progress!
+//             </Text>
+//             <GalleryBuddiesWorkout />
+//           </View>
+//           <View>
+//             <Text style={styles.titleText}>people you might know</Text>
+//             <GalleryPeopleYouMightKnow />
+//           </View>
+//           <View>
+//             <Text style={styles.titleText}>whats new</Text>
+//             <Text style={styles.subTitleText}>
+//               Checkout the lates news on MATE-UP
+//             </Text>
+//             <CardMeetTheMemberOfTheMonth />
+//             <GalleryEducationalContent />
+//           </View>
+//           <View style={{ alignItems: "center", marginBottom: 200 }}>
+//             <AppButton title={"see all"} width={222} height={45} />
+//           </View>
+//         </Screen>
+//       </ScrollView>
+//       <View style={styles.fixButtonPosition}>
+//         <AppButtonBorder
+//           image={require("../../../assets/icons/calendar.png")}
+//           title="calendar"
+//         />
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: colors.blackBc,
+//     flex: 1,
+//   },
+//   welcomText: {
+//     fontFamily: "montserrat-black",
+//     fontSize: 48,
+//     color: colors.white,
+//     marginBottom: 10,
+//     paddingLeft: 16,
+//   },
+//   fixButtonPosition: {
+//     position: "absolute",
+//     zIndex: 1,
+//     right: 16,
+//     top: "75%",
+//     buttom: 0,
+//   },
+//   buddiesWorkoutContainer: {
+//     marginTop: 96,
+//   },
+//   titleText: {
+//     fontFamily: "montserrat-black",
+//     fontSize: 26,
+//     color: colors.orangePrimary,
+//     marginBottom: 10,
+//     textTransform: "uppercase",
+//     paddingLeft: 16,
+//   },
+//   subTitleText: {
+//     fontFamily: "nunitoSans-regular",
+//     fontSize: 14,
+//     color: colors.white,
+//     marginBottom: 32,
+//     paddingLeft: 16,
+//   },
+// });
+
 export default function MainHomeScreen() {
-  return (
+  const data = [
+    { type: "GalleryBuddies" },
+    { type: "Line" },
+    { type: "Text", content: "WELCOME", name: "REZA" },
+    { type: "UserNextWorkoutPlanning" },
+    { type: "GalleryBuddiesWorkout" },
+    { type: "GalleryPeopleYouMightKnow" },
+    { type: "CardMeetTheMemberOfTheMonth" },
+    { type: "GalleryEducationalContent" },
+  ];
+
+  //The `renderItemCache` object is created to map each component
+  // type to its corresponding rendering function.
+  const renderItemCache = {};
+
+  //The `renderItem` function is defined to render the components based
+  // on the `data` array.
+  const renderItem = ({ item }) => renderItemCache[item.type](item);
+  renderItemCache["GalleryBuddies"] = (item) => (
+    <GalleryBuddies style={{ paddingLeft: 16, marginBottom: 40 }} item={item} />
+  );
+  renderItemCache["Line"] = (item) => <Line marginBottom={40} item={item} />;
+
+  //TODO: make this a s an component that can be reused, for name
+  renderItemCache["Text"] = ({ content, name }) => (
+    <Text style={styles.welcomeText}>
+      {content} {name}
+    </Text>
+  );
+  renderItemCache["UserNextWorkoutPlanning"] = (item) => (
+    <UserNextWorkoutPlanning item={item} />
+  );
+
+  renderItemCache["GalleryBuddiesWorkout"] = (item) => (
+    <View style={styles.buddiesWorkoutContainer}>
+      <Text style={styles.titleText}>upcoming buddies workout</Text>
+      <Text style={styles.subTitleText}>
+        Discover your buddies' latest workout routines, Join in for Fun and
+        Progress!
+      </Text>
+      <GalleryBuddiesWorkout item={item} />
+    </View>
+  );
+  renderItemCache["GalleryPeopleYouMightKnow"] = (item) => (
     <View>
-      <ScrollView>
-        <Screen style={styles.container}>
-          <GalleryBuddies style={{ paddingLeft: 16, marginBottom: 40 }} />
-          <Line marginBottom={40} />
-          <Text style={styles.welcomText}>WELCOME REZA</Text>
-          <UserNextWorkoutPlanning />
-          <View style={styles.buddiesWorkoutContainer}>
-            <Text style={styles.titleText}>upcoming buddies workout</Text>
-            <Text style={styles.subTitleText}>
-              Discover your buddies' latest workout routines, Join in for Fun
-              and Progress!
-            </Text>
-            <GalleryBuddiesWorkout />
-          </View>
-          <View>
-            <Text style={styles.titleText}>people you might know</Text>
-            <GalleryPeopleYouMightKnow />
-          </View>
-          <View>
-            <Text style={styles.titleText}>whats new</Text>
-            <Text style={styles.subTitleText}>
-              Checkout the lates news on MATE-UP
-            </Text>
-            <CardMeetTheMemberOfTheMonth />
-            <GalleryEducationalContent />
-          </View>
-          <View style={{ alignItems: "center", marginBottom: 200 }}>
-            <AppButton title={"see all"} width={222} height={45} />
-          </View>
-        </Screen>
-      </ScrollView>
+      <Text style={styles.titleText}>people you might know</Text>
+      <GalleryPeopleYouMightKnow item={item} />
+    </View>
+  );
+  renderItemCache["CardMeetTheMemberOfTheMonth"] = (item) => (
+    <View>
+      <Text style={styles.titleText}>whats new</Text>
+      <Text style={styles.subTitleText}>
+        Checkout the lates news on MATE-UP
+      </Text>
+      <CardMeetTheMemberOfTheMonth item={item} />
+    </View>
+  );
+  renderItemCache["GalleryEducationalContent"] = (item) => (
+    <View>
+      <GalleryEducationalContent item={item} />
+      <View style={styles.buttonSeeAll}>
+        <AppButton title={"see all"} width={222} height={45} />
+      </View>
+    </View>
+  );
+
+  return (
+    <Screen style={styles.container}>
+      {/* The `FlatList` component is used to render the components based on the
+       `data` array, using the `renderItem` function. */}
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item, id) => id.toString()}
+        showsHorizontalScrollIndicator={false}
+      />
       <View style={styles.fixButtonPosition}>
         <AppButtonBorder
           image={require("../../../assets/icons/calendar.png")}
           title="calendar"
         />
       </View>
-    </View>
+    </Screen>
   );
 }
 
@@ -62,19 +189,12 @@ const styles = StyleSheet.create({
     backgroundColor: colors.blackBc,
     flex: 1,
   },
-  welcomText: {
+  welcomeText: {
     fontFamily: "montserrat-black",
     fontSize: 48,
     color: colors.white,
     marginBottom: 10,
     paddingLeft: 16,
-  },
-  fixButtonPosition: {
-    position: "absolute",
-    zIndex: 1,
-    right: 16,
-    top: "75%",
-    buttom: 0,
   },
   buddiesWorkoutContainer: {
     marginTop: 96,
@@ -93,5 +213,21 @@ const styles = StyleSheet.create({
     color: colors.white,
     marginBottom: 32,
     paddingLeft: 16,
+  },
+  buddiesWorkoutContainer: {
+    marginTop: 96,
+  },
+  fixButtonPosition: {
+    position: "absolute",
+    zIndex: 1,
+    right: 16,
+    top: "75%",
+    bottom: 0,
+  },
+  buttonSeeAll: {
+    marginTop: 32,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 32,
   },
 });
