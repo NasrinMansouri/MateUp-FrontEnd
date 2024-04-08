07763692917c69df56669e5c150ced95d92f67e4
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-08 09:12:36 +0200

    test-1 font component in app

--- Front_End/App.js
+++ Front_End/App.js
@@ -1,59 +1,100 @@
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
+// import { useFonts } from "expo-font";
+
+// //for fonts
+// import * as SplashScreen from "expo-splash-screen";
+// import { useCallback } from "react";
+// SplashScreen.preventAutoHideAsync();
+
+// import colors from "./app/config/colors";
+// import AppButtonBorder from "./app/components/AppButtonBorder";
+
+// import DonutChart from "./app/components/challenge/DonutChart";
+// import StopWatch from "./app/components/challenge/StopWatch";
+// import GalleryAllChallenges from "./app/components/challenge/GalleryAllChallenges";
+// import MainHomeScreen from "./app/screens/home/MainHomeScreen";
+
+// export default function App() {
+//   //for fonts
+//   const [isLoaded] = useFonts({
+//     "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
+//     "nunitoSans-bold": require("./assets/fonts/NunitoSans7pt-Bold.ttf"),
+//     "nunitoSans-regular": require("./assets/fonts/NunitoSans7pt-Regular.ttf"),
+//     "nunitoSans-extraBold": require("./assets/fonts/NunitoSans7pt-ExtraBold.ttf"),
+//   });
+//   const handleOnLayout = useCallback(async () => {
+//     if (isLoaded) {
+//       await SplashScreen.hideAsync(); //hide the splashscreen
+//     }
+//   }, [isLoaded]);
+//   if (!isLoaded) {
+//     return null;
+//   }
+
+//   return (
+//     <View style={styles.container} onLayout={handleOnLayout}>
+//       <ScrollView style={styles.View}>
+//         <View>
+//           {/* <DonutChart /> */}
+//           {/* <StopWatch /> */}
+//           <GalleryAllChallenges />
+//           {/* <MainHomeScreen /> */}
+//         </View>
+//         <StatusBar style="auto" />
+//       </ScrollView>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: colors.blackBc,
+
+//     // paddingTop: 50,
+//     // paddingLeft: 16,
+//   },
+//   // View: {
+//   //   flex: 1,
+//   // },
+// });
+
+// //for fix btn
+// {
+//   /* <View
+//         style={{
+//           // marginBottom: 100,
+//           position: "absolute",
+//           top: "75%",
+//           right: 24,
+//           zIndex: 1,
+//         }}
+//       >
+//         <AppButtonBorder
+//           image={require("./assets/icons/calendar.png")}
+//           title="calendar"
+//         />
+//       </View> */
+// }
+
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
-import { useFonts } from "expo-font";
+import { StyleSheet, View, ScrollView } from "react-native";
 
-//for fonts
-import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
-SplashScreen.preventAutoHideAsync();
-
+import FontLoader from "./app/components/FontLoader";
+import GalleryAllChallenges from "./app/components/challenge/GalleryAllChallenges";
 import colors from "./app/config/colors";
-import AppButtonBorder from "./app/components/AppButtonBorder";
-
-import DonutChart from "./app/components/challenge/DonutChart";
-import StopWatch from "./app/components/challenge/StopWatch";
 
 export default function App() {
-  //for fonts
-  const [isLoaded] = useFonts({
-    "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
-    "nunitoSans-bold": require("./assets/fonts/NunitoSans7pt-Bold.ttf"),
-    "nunitoSans-regular": require("./assets/fonts/NunitoSans7pt-Regular.ttf"),
-    "nunitoSans-extraBold": require("./assets/fonts/NunitoSans7pt-ExtraBold.ttf"),
-  });
-  const handleOnLayout = useCallback(async () => {
-    if (isLoaded) {
-      await SplashScreen.hideAsync(); //hide the splashscreen
-    }
-  }, [isLoaded]);
-  if (!isLoaded) {
-    return null;
-  }
-
   return (
-    <View style={styles.container} onLayout={handleOnLayout}>
-      <ScrollView style={styles.View}>
-        <View>
-          <DonutChart />
-          <StopWatch />
-        </View>
-        <StatusBar style="auto" />
-      </ScrollView>
-      <View
-        style={{
-          // marginBottom: 100,
-          position: "absolute",
-          top: "75%",
-          right: 24,
-          zIndex: 1,
-        }}
-      >
-        <AppButtonBorder
-          image={require("./assets/icons/calendar.png")}
-          title="calendar"
-        />
+    <FontLoader>
+      <View style={styles.container}>
+        <ScrollView style={styles.view}>
+          <GalleryAllChallenges />
+          <StatusBar style="auto" />
+        </ScrollView>
       </View>
-    </View>
+    </FontLoader>
   );
 }
 
@@ -61,10 +102,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.blackBc,
-    paddingTop: 50,
-    // paddingLeft: 16,
   },
-  View: {
+  view: {
     flex: 1,
   },
 });
