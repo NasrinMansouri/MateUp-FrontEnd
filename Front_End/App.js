edf7a878aee29057ca8caf56140fa000896fb305
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-09 08:03:11 +0200

    bring font to app.js, because when i used it in seperator component i used to get error

--- Front_End/App.js
+++ Front_End/App.js
@@ -1,98 +1,73 @@
-// import { StatusBar } from "expo-status-bar";
-// import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
-// import { useFonts } from "expo-font";
-
-// //for fonts
-// import * as SplashScreen from "expo-splash-screen";
-// import { useCallback } from "react";
-// SplashScreen.preventAutoHideAsync();
-
-// import colors from "./app/config/colors";
-// import AppButtonBorder from "./app/components/AppButtonBorder";
-
-// export default function App() {
-//   //for fonts
-//   const [isLoaded] = useFonts({
-//     "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
-//     "nunitoSans-bold": require("./assets/fonts/NunitoSans7pt-Bold.ttf"),
-//     "nunitoSans-regular": require("./assets/fonts/NunitoSans7pt-Regular.ttf"),
-//     "nunitoSans-extraBold": require("./assets/fonts/NunitoSans7pt-ExtraBold.ttf"),
-//   });
-//   const handleOnLayout = useCallback(async () => {
-//     if (isLoaded) {
-//       await SplashScreen.hideAsync(); //hide the splashscreen
-//     }
-//   }, [isLoaded]);
-//   if (!isLoaded) {
-//     return null;
-//   }
-
-//   return (
-//     <View style={styles.container} onLayout={handleOnLayout}>
-//       <ScrollView style={styles.View}>
-//       // //for fix btn
-// {
-//   /* <View
-//         style={{
-//           // marginBottom: 100,
-//           position: "absolute",
-//           top: "75%",
-//           right: 24,
-//           zIndex: 1,
-//         }}
-//       >
-//         <AppButtonBorder
-//           image={require("./assets/icons/calendar.png")}
-//           title="calendar"
-//         />
-//       </View> */
-// }
-//         <StatusBar style="auto" />
-//       </ScrollView>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: colors.blackBc,
-
-//     // paddingTop: 50,
-//     // paddingLeft: 16,
-//   },
-//   // View: {
-//   //   flex: 1,
-//   // },
-// });
-
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
+//for fonts
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+SplashScreen.preventAutoHideAsync();
 
-import FontLoader from "./app/components/FontLoader";
-import TopNav from "./app/components/TopNav";
 import colors from "./app/config/colors";
 import MainHomeScreen from "./app/screens/home/MainHomeScreen";
 
 export default function App() {
+  //for fonts
+  const [isLoaded] = useFonts({
+    "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
+    "nunitoSans-bold": require("./assets/fonts/NunitoSans7pt-Bold.ttf"),
+    "nunitoSans-regular": require("./assets/fonts/NunitoSans7pt-Regular.ttf"),
+    "nunitoSans-extraBold": require("./assets/fonts/NunitoSans7pt-ExtraBold.ttf"),
+  });
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync(); //hide the splashscreen
+    }
+  }, [isLoaded]);
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <FontLoader>
-      <View style={styles.container}>
-        {/* <TopNav
-          // showProfilePic={true}
-          // userImage={require("./assets/person2.jpg")}
-          showSearchBar={true}
-        /> */}
-        <MainHomeScreen />
-        <StatusBar style="auto" />
-      </View>
-    </FontLoader>
+    <View style={styles.container} onLayout={handleOnLayout}>
+      <MainHomeScreen />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.blackBc,
     flex: 1,
+    backgroundColor: colors.blackBc,
   },
 });
+
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, View, Text } from "react-native";
+
+// import FontLoader from "./app/components/FontLoader";
+// import TopNav from "./app/components/TopNav";
+// import colors from "./app/config/colors";
+// import MainHomeScreen from "./app/screens/home/MainHomeScreen";
+
+// export default function App() {
+//   return (
+//     <FontLoader>
+//       <View style={styles.container}>
+//         {/* <TopNav
+//           // showProfilePic={true}
+//           // userImage={require("./assets/person2.jpg")}
+//           showSearchBar={true}
+//         /> */}
+//         <MainHomeScreen />
+//         <StatusBar style="auto" />
+//       </View>
+//     </FontLoader>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: colors.blackBc,
+//     flex: 1,
+//   },
+// });
