c7bea026bfbb5088512d7ac5e832ce6cfaf85dee
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-09 17:46:45 +0200

    delete function, to delet notification from notifications

--- Front_End/app/components/ListItemDeletAction.js
+++ Front_End/app/components/ListItemDeletAction.js
@@ -3,12 +3,15 @@ import React from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../config/colors";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
-export default function ListItemDeletAction(props) {
+export default function ListItemDeletAction({ onPress }) {
   return (
-    <View style={styles.container}>
-      <MaterialCommunityIcons name="trash-can" size={24} color="white" />
-    </View>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons name="trash-can" size={24} color="white" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -16,6 +19,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.danger,
     width: 70,
+    height: "100%",
     justifyContent: "center",
     alignItems: "center",
   },
