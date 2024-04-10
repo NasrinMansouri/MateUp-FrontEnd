fed7f2a4a1013ec3c539b977d764cbeb576b251d
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-10 10:43:52 +0200

    appPicker, adds gesture... to the root

--- Front_End/app/components/AppPicker.js
+++ Front_End/app/components/AppPicker.js
@@ -5,15 +5,19 @@ import {
   TouchableWithoutFeedback,
   Modal,
   Button,
+  FlatList,
 } from "react-native";
 import React, { useState } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import colors from "../config/colors";
 import Screen from "./Screen";
+import PickerItem from "./PickerItem";
 
 export default function AppPicker({
   icon,
+  items,
   borderWidth = 1,
   borderColor = colors.grayLight50,
   backgroundColor = colors.blackBc,
@@ -24,38 +28,50 @@ export default function AppPicker({
 
   return (
     <>
-      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
-        <View
-          style={[
-            styles.container,
-            {
-              borderColor: borderColor,
-              backgroundColor: backgroundColor,
-              borderWidth: borderWidth,
-            },
-          ]}
-        >
-          {icon && (
+      <GestureHandlerRootView>
+        <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+          <View
+            style={[
+              styles.container,
+              {
+                borderColor: borderColor,
+                backgroundColor: backgroundColor,
+                borderWidth: borderWidth,
+              },
+            ]}
+          >
+            {icon && (
+              <MaterialCommunityIcons
+                name={icon}
+                size={20}
+                color={colors.grayLight50}
+                style={styles.icon}
+              />
+            )}
+            <Text style={styles.text}> {placeholder} </Text>
             <MaterialCommunityIcons
-              name={icon}
-              size={20}
+              name="chevron-down"
+              size={24}
               color={colors.grayLight50}
-              style={styles.icon}
             />
-          )}
-          <Text style={styles.text}> {placeholder} </Text>
-          <MaterialCommunityIcons
-            name="chevron-down"
-            size={24}
-            color={colors.grayLight50}
-          />
-        </View>
-      </TouchableWithoutFeedback>
-      <Modal visible={modalVisible} animationType="slide">
-        <Screen>
-          <Button title="Close" onPress={() => setModalVisible(false)} />
-        </Screen>
-      </Modal>
+          </View>
+        </TouchableWithoutFeedback>
+        <Modal visible={modalVisible} animationType="slide">
+          <Screen>
+            <Button title="Close" onPress={() => setModalVisible(false)} />
+            <FlatList
+              data={items}
+              keyExtractor={(item) => item.value}
+              renderItem={({ item }) => (
+                <PickerItem
+                  label={item.label}
+                  onPress={() => console.log(item)}
+                />
+              )}
+            />
+          </Screen>
+        </Modal>
+      </GestureHandlerRootView>
     </>
   );
 }
