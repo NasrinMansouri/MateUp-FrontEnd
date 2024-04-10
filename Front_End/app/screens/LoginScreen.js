15dd25832d9cac8f721aa3149c7d2ca7a6e8998b
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-10 13:12:55 +0200

    build a better form using Formik library

--- Front_End/app/screens/LoginScreen.js
+++ Front_End/app/screens/LoginScreen.js
@@ -1,43 +1,59 @@
 import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
-import React, { useState } from "react";
+import React from "react";
+import { Formik } from "formik";
 
 import AppTextInpure from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
+import { Entypo } from "@expo/vector-icons";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-
   return (
     <Screen style={styles.container}>
       <TouchableWithoutFeedback>
-        <Text style={styles.helpText}>Need help?</Text>
+        <Text style={styles.helpText}>Need help ?</Text>
       </TouchableWithoutFeedback>
-      <AppTextInpure
-        autoCapitalize="none"
-        autoCorrect={false}
-        icon="email"
-        keyboardType="email-address"
-        onChangeText={(text) => setEmail(text)}
-        placeholder="Enter Email"
-        textContentType="emailAddress" //only work on ios
-      />
-      <AppTextInpure
-        autoCapitalize="none"
-        autoCorrect={false}
-        icon="lock"
-        onChangeText={(text) => setPassword(text)}
-        placeholder="Enter Password"
-        secureTextEntry={true}
-        textContentType="password" //only work on ios
-      />
-      <AppButton
-        title="Sign In"
-        height={48}
-        onPress={() => console.log(email, password)}
-      />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <>
+            <AppTextInpure
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              keyboardType="email-address"
+              onChangeText={handleChange("email")}
+              placeholder="Enter Email"
+              textContentType="emailAddress" //only work on ios
+            />
+            <AppTextInpure
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              onChangeText={handleChange("password")}
+              placeholder="Enter Password"
+              secureTextEntry={true}
+              textContentType="password" //only work on ios
+            />
+            <View>
+              <AppButton title="Sign In" height={55} onPress={handleSubmit} />
+              <TouchableWithoutFeedback onPress={handleSubmit}>
+                <View style={styles.lastBtnContainer}>
+                  <Text style={styles.ForgetPassText}>Not A Member Yet ? </Text>
+                  <Entypo
+                    name="chevron-right"
+                    size={24}
+                    color={colors.orangeSecondary}
+                  />
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </>
+        )}
+      </Formik>
     </Screen>
   );
 }
@@ -54,5 +70,19 @@ const styles = StyleSheet.create({
     marginRight: 16,
     alignSelf: "flex-end",
     textTransform: "uppercase",
+  },
+  ForgetPassText: {
+    color: colors.white,
+    fontFamily: "montserrat-black",
+    fontSize: 14,
+    alignSelf: "center",
+    textTransform: "uppercase",
+  },
+  lastBtnContainer: {
+    marginTop: 16,
+    marginRight: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
