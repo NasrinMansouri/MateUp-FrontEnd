0f59bbb3b9e7d706c02df4c31f3427c678923732
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-22 16:46:13 +0200

    makes the background for item seperator as prop

--- Front_End/app/components/lists/ListItemSeperator.js
+++ Front_End/app/components/lists/ListItemSeperator.js
@@ -3,15 +3,17 @@ import { StyleSheet, View } from "react-native";
 
 import colors from "../../config/colors";
 
-function ListItemSeparator() {
-  return <View style={styles.separator} />;
+function ListItemSeparator({ backgroundColor = colors.grayLight50 }) {
+  return (
+    <View style={[styles.separator, { backgroundColor: backgroundColor }]} />
+  );
 }
 
 const styles = StyleSheet.create({
   separator: {
     width: "100%",
     height: 1,
-    backgroundColor: colors.grayLight50,
+    // backgroundColor: colors.grayLight50,
   },
 });
 
