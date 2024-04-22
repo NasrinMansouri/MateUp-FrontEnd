4d73691e07b2f63060866339d3cb5c46abc37c40
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-22 09:54:28 +0200

    export other component to index.js for home component

--- Front_End/app/components/home/index.js
+++ Front_End/app/components/home/index.js
@@ -3,3 +3,5 @@ export { default as GalleryPeopleYouMightKnow } from "./GalleryPeopleYouMightKno
 export { default as CardMeetTheMemberOfTheMonth } from "./CardMeetTheMemberOfTheMonth";
 export { default as GalleryBuddiesWorkout } from "./GalleryBuddiesWorkout";
 export { default as UserNextWorkoutPlanning } from "./UserNextWorkoutPlanning";
+export { default as DisplayBuddies } from "./DisplayBuddies";
+export { default as AddBuddyBtn } from "./AddBuddyBtn";
