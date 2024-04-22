f6932b000874a12fd068e8a3a928cd0d87ae523a
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-22 16:54:07 +0200

    build index file for buddy components

---
+++ Front_End/app/components/buddy/index.js
@@ -0,0 +1,6 @@
+export { default as CardMatchBasedWorkout } from "./CardMatchBasedWorkout";
+export { default as CardMatchClubMembers } from "./CardMatchClubMembers";
+export { default as GalleryBuddies } from "./GalleryBuddies";
+export { default as GalleryConnectAll } from "./GalleryConnectAll";
+export { default as GalleryMatchBasedWorkout } from "./GalleryMatchBasedWorkout";
+export { default as GalleryMatchClubMembers } from "./GalleryMatchClubMembers";
