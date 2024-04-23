93c9af6ccd246b52266286ab7468758de701d5fa
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-23 16:05:21 +0200

    adds a header as atitle to gallery buddy component

--- Front_End/app/components/buddy/GalleryBuddies.js
+++ Front_End/app/components/buddy/GalleryBuddies.js
@@ -3,51 +3,56 @@
 // used on top of home page
 
 import React from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View, Text } from "react-native";
 
 import CardProfile from "../CardProfile";
 import colors from "../../config/colors";
 
-function GalleryBuddies({ buddies, style }) {
+function GalleryBuddies({ buddies, style, header }) {
   const capitalizeFirstLetter = (string) => {
     // capitalize first letter and make rest lowercase
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
   };
   //dummy data for testing
-  const buddiesData = [
-    {
-      id: 1,
-      name: "MMMMMMMMMMMMMMMM ",
-      image: require("../../../assets/person-1.jpg"),
-    },
-    {
-      id: 2,
-      name: "Coucheeeeeeee ",
-      image: require("../../../assets/person-1.jpg"),
-    },
-    {
-      id: 3,
-      name: "Couchhhhhhhhh ",
-      image: require("../../../assets/person-1.jpg"),
-    },
-    {
-      id: 4,
-      name: "NNNNNN NNNNN ",
-      image: require("../../../assets/person-1.jpg"),
-    },
-    {
-      id: 5,
-      name: "Couch ",
-      image: require("../../../assets/person-1.jpg"),
-    },
-  ];
+  // const buddiesData = [
+  //   {
+  //     id: 1,
+  //     name: "MMMMMMMMMMMMMMMM ",
+  //     image: require("../../../assets/person-1.jpg"),
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Coucheeeeeeee ",
+  //     image: require("../../../assets/person-1.jpg"),
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Couchhhhhhhhh ",
+  //     image: require("../../../assets/person-1.jpg"),
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "NNNNNN NNNNN ",
+  //     image: require("../../../assets/person-1.jpg"),
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Couch ",
+  //     image: require("../../../assets/person-1.jpg"),
+  //   },
+  // ];
   return (
     <View>
+      {header && (
+        <View>
+          <Text style={styles.header}>{header}</Text>
+        </View>
+      )}
       <FlatList
         style={[styles.container, style]}
         horizontal
         showsHorizontalScrollIndicator={false}
-        data={buddiesData}
+        data={buddies}
         keyExtractor={(buddies) => buddies.id.toString()}
         renderItem={({ item }) => (
           <CardProfile
@@ -76,6 +81,14 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.blackBc,
     paddingLeft: 16,
+  },
+  header: {
+    fontFamily: "montserrat-black",
+    color: colors.orangePrimary,
+    fontSize: 32,
+    textTransform: "uppercase",
+    marginBottom: 8,
+    marginLeft: 16,
   },
 });
 export default GalleryBuddies;
