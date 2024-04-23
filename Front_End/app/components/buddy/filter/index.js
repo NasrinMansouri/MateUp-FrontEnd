3ff18203ebfc6bb9cd96f804a9bc3fa9e47bc773
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-23 10:43:40 +0200

    index includes more component for filter modal

--- Front_End/app/components/buddy/filter/index.js
+++ Front_End/app/components/buddy/filter/index.js
@@ -1,2 +1,4 @@
 export { default as Gender } from "./Gender";
 export { default as ModalHeader } from "./ModalHeader";
+export { default as DropDownList } from "./DropDownList";
+export { default as DatePicker } from "./DatePicker";
