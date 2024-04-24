dc244c48ce31bcc6b768c5493a98a67a8a101d06
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-24 19:37:14 +0200

    build gallery joined challenge

---
+++ Front_End/app/components/challenge/GalleryJoinedChallenge.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+import CardJoinedChallenge from "./CardJoinedChallenge";
+import colors from "../../config/colors";
+
+export default function GalleryJoinedChallenge({
+  joinedChallenge,
+  header,
+  fontSize = 28,
+}) {
+  return (
+    <View style={styles.mainContainer}>
+      {header && (
+        <Text style={[styles.title, { fontSize: fontSize }]}>{header}</Text>
+      )}
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        style={styles.container}
+        horizontal
+        data={joinedChallenge}
+        keyExtractor={(joinedChallenge) => joinedChallenge.id.toString()}
+        renderItem={({ item }) => (
+          <CardJoinedChallenge
+            challenegImage={item.challenegImage}
+            challengeName={item.challengeName}
+            challengeGoal={item.challengeGoal}
+            startDate={item.startDate}
+            endDate={item.endDate}
+            year={item.year}
+            onPress={item.onPress}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  //   mainContainer: {
+  //     marginTop: 96,
+  //   },
+  container: {
+    paddingTop: 16,
+    paddingLeft: 16,
+  },
+  title: {
+    fontFamily: "montserrat-black",
+    // fontSize: 26,
+    color: colors.orangePrimary,
+    marginBottom: 10,
+    textTransform: "uppercase",
+    paddingLeft: 16,
+  },
+});
