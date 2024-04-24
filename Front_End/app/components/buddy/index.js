987133ba74259ebf93fe0e2402358e3b75f9a021
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-24 19:51:15 +0200

    adds component to index and modify import in buddy profile screen

--- Front_End/app/components/buddy/index.js
+++ Front_End/app/components/buddy/index.js
@@ -4,3 +4,5 @@ export { default as GalleryBuddies } from "./GalleryBuddies";
 export { default as GalleryConnectAll } from "./GalleryConnectAll";
 export { default as GalleryMatchBasedWorkout } from "./GalleryMatchBasedWorkout";
 export { default as GalleryMatchClubMembers } from "./GalleryMatchClubMembers";
+export { default as RequestCalendarAccess } from "./RequestCalendarAccess";
+export { default as FilterModal } from "./FilterModal";
