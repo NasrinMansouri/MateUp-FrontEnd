d9dfb73d11890f41b35d0c54b0c8362c3e3beba9
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-26 12:36:23 +0200

    user image componenet is now accepting all style as prop

--- Front_End/app/components/shareMemberProfile/UserImage.js
+++ Front_End/app/components/shareMemberProfile/UserImage.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Image } from "react-native";
 
-export default function UserImage({ userImage }) {
+export default function UserImage({
+  userImage,
+  imageWidth,
+  imageHeight,
+  imageRadius,
+  marginLeft,
+  marginTop,
+  marginBottom,
+}) {
+  // const [calculateWidth, setCalculateWidth] = useState(imageWidth);
+  // const [calculateHeight, setCalculateHeight] = useState(imageHeight);
+
+  // useEffect(() => {
+  //   Image.getSize(userImage.uri, (width, height) => {
+  //     const aspectRatio = width / height;
+  //     if (aspectRatio > 1) {
+  //       // image is landscape
+  //       setCalculateWidth(imageWidth);
+  //       setCalculateHeight(imageWidth / aspectRatio);
+  //     } else {
+  //       // image is portrait
+  //       setCalculateWidth(imageHeight * aspectRatio);
+  //       setCalculateHeight(imageHeight);
+  //     }
+  //   });
+  // }, [userImage.uri, imageHeight, imageWidth]);
   return (
     <View style={styles.container}>
-      <Image source={userImage} style={styles.userImage} />
+      <Image
+        resizeMode="cover"
+        source={userImage}
+        style={[
+          styles.userImage,
+          {
+            width: imageWidth,
+            height: imageHeight,
+            borderRadius: imageRadius,
+            marginLeft: marginLeft,
+            marginTop: marginTop,
+            marginBottom: marginBottom,
+          },
+        ]}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {},
-  userImage: {
-    width: 116,
-    height: 116,
-    borderRadius: 116 / 2,
-    marginLeft: 16,
-    marginTop: 40,
-    // marginBottom: 22,
-  },
+  userImage: {},
 });
