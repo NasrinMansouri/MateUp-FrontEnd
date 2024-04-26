1b776b902a1cb5a3337335b32c47cecfd57209de
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-26 10:36:42 +0200

    adds omponent to index

--- Front_End/app/components/coach/index.js
+++ Front_End/app/components/coach/index.js
@@ -1,4 +1,4 @@
 export { default as CardAllCoaches } from "./CardAllCoaches";
 export { default as GalleryAllCoaches } from "./GalleryAllCoaches";
 export { default as GalleryCoachesClubMembers } from "./GalleryCoachesClubMembers";
-export { default as CardCoachClubMembers } from "./CardCoachClubMembers";
+export { default as CardCoachClubMembers } from "./CardCoachClubMember";
