872469f81222786e6f2479562769ee9ede9f5396
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-29 09:16:03 +0200

    expot component to index ,coach

--- Front_End/app/components/coach/index.js
+++ Front_End/app/components/coach/index.js
@@ -1,4 +1,8 @@
 export { default as CardAllCoaches } from "./CardAllCoaches";
+export { default as CardCoachClubMembers } from "./CardCoachClubMember";
+export { default as CardMySession } from "./CardMySession";
+export { default as DisplayVideo } from "./DisplayVideo";
 export { default as GalleryAllCoaches } from "./GalleryAllCoaches";
 export { default as GalleryCoachesClubMembers } from "./GalleryCoachesClubMembers";
-export { default as CardCoachClubMembers } from "./CardCoachClubMember";
+export { default as GalleryDisplayVideos } from "./GalleryDisplayVideos";
+export { default as TitleSubtitle } from "./TitleSubtitle";
