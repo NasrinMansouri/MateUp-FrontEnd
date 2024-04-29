eca9eda9d2d4f3fd67c48a9ed73865505b6d2815
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-29 20:58:45 +0200

    builds challnege description component

---
+++ Front_End/app/components/challenge/ChallengeDescription.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+
+import colors from "../../config/colors";
+
+export default function ChallengeDescription({
+  challengeDescription,
+  marginBottom,
+  marginTop,
+}) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <>
+      <View
+        style={[
+          styles.descriptionContainer,
+          { marginBottom: marginBottom, marginTop: marginTop },
+        ]}
+      >
+        <Text
+          style={styles.description}
+          numberOfLines={expanded ? undefined : 3}
+        >
+          {challengeDescription}
+        </Text>
+      </View>
+      {challengeDescription.length > 150 && (
+        <TouchableOpacity onPress={() => setExpanded(!expanded)}>
+          <Text style={styles.moreAsText}>{expanded ? "less" : "more"}</Text>
+        </TouchableOpacity>
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 16,
+    fontFamily: "nunitoSans-regular",
+    color: colors.white,
+  },
+  moreAsText: {
+    fontSize: 16,
+    fontFamily: "nunitoSans-extraBold",
+    color: colors.grayLight100,
+    marginBottom: 40,
+  },
+});
