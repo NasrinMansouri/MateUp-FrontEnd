8f1deabc6b25f007e5f061292a7658e581ed5440
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-29 13:24:16 +0200

    builds challenge screen

---
+++ Front_End/app/screens/challenge/ChallengeScreen.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  LayoutAnimation,
+  UIManager,
+  Platform,
+  FlatList,
+} from "react-native";
+import Screen from "../../components/Screen";
+import colors from "../../config/colors";
+import TopNav from "../../components/topNavigation/TopNav";
+import Join from "./challengeScreen/Join";
+import MyChallenges from "./challengeScreen/MyChallenges";
+import TabButton from "../../components/tabButtons/TabButtons";
+
+// For android to enable Layoutanimation
+if (Platform.OS === "android") {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
+
+const data = [{ key: "join" }, { key: "my challenges" }];
+
+export default function ChallengeScreen({}) {
+  // to keep track of which button is active
+  const [activeContent, setActiveContent] = useState("join");
+
+  useEffect(() => {
+    LayoutAnimation.easeInEaseOut();
+  }, [activeContent]);
+
+  // shows the content according to the button pressed
+  const handleJoin = () => {
+    setActiveContent("join");
+  };
+  const handleMyChallenges = () => {
+    setActiveContent("my challenges");
+  };
+
+  return (
+    <Screen>
+      <TopNav showSearchBar={true} />
+      {/* <Text style={styles.title}>group challenges</Text> */}
+      <View style={styles.topBtnscontainer}>
+        <TabButton
+          text="Join"
+          active={activeContent === "join"}
+          onPress={handleJoin}
+        />
+        <TabButton
+          text="My Challenges"
+          active={activeContent === "my challenges"}
+          onPress={handleMyChallenges}
+        />
+      </View>
+      {/* content */}
+      <View style={styles.contentContainer}>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.key}
+          renderItem={({ item }) => {
+            return (
+              <View>
+                {item.key === "join" && activeContent === "join" && <Join />}
+                {item.key === "my challenges" &&
+                  activeContent === "my challenges" && <MyChallenges />}
+              </View>
+            );
+          }}
+        />
+      </View>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  topBtnscontainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 40,
+    // marginBottom: 20,
+  },
+  contentContainer: {
+    flex: 1,
+    marginTop: 10,
+  },
+  title: {
+    fontFamily: "montserrat-black",
+    fontSize: 22,
+    color: colors.white,
+    textTransform: "uppercase",
+    alignSelf: "center",
+    marginTop: 30,
+  },
+});
