57f998c6073f99db31986af4fabe8f07a4ea498e
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-04-29 09:07:30 +0200

    builds coach profile screen

--- Front_End/app/screens/coach/CoachProfileScreen.js
+++ Front_End/app/screens/coach/CoachProfileScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, StyleSheet, View, Text } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 
 import AppButton from "../../components/AppButton";
 import colors from "../../config/colors";
