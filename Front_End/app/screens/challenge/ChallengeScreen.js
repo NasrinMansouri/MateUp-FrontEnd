2cbffcaa8d40040540e7802a4c9dfa3d593fbb14
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-05-07 10:09:14 +0200

    coach and challenge screen now using top tab navigation, instead of conditionally rendering different content, cleaner

--- Front_End/app/screens/challenge/ChallengeScreen.js
+++ Front_End/app/screens/challenge/ChallengeScreen.js
@@ -26,9 +26,11 @@ const ChallengeScreen = ({ navigation }) => {
           tabBarLabelStyle: {
             fontFamily: "montserrat-black",
             fontSize: 14,
-            color: colors.white,
           },
-          tabBarStyle: { backgroundColor: colors.blackBc },
+          tabBarIndicatorStyle: { backgroundColor: colors.green, height: 3 },
+          tabBarActiveTintColor: colors.green,
+          tabBarInactiveTintColor: colors.grayLight50,
+          tabBarStyle: { backgroundColor: colors.blackBc, height: 42 },
         }}
       >
         <Tab.Screen name="Join" component={Join} />
@@ -46,104 +48,3 @@ const styles = StyleSheet.create({
 });
 
 export default ChallengeScreen;
-
-// import React, { useState, useEffect } from "react";
-// import {
-//   StyleSheet,
-//   Text,
-//   View,
-//   LayoutAnimation,
-//   UIManager,
-//   Platform,
-//   FlatList,
-// } from "react-native";
-// import Screen from "../../components/Screen";
-// import colors from "../../config/colors";
-// import TopNav from "../../components/topNavigation/TopNav";
-// import TabButton from "../../components/tabButtons/TabButtons";
-// import { Join, MyChallenges } from "./challengeScreen";
-
-// // For android to enable Layoutanimation
-// if (Platform.OS === "android") {
-//   if (UIManager.setLayoutAnimationEnabledExperimental) {
-//     UIManager.setLayoutAnimationEnabledExperimental(true);
-//   }
-// }
-
-// const data = [{ key: "join" }, { key: "my challenges" }];
-
-// export default function ChallengeScreen({}) {
-//   // to keep track of which button is active
-//   const [activeContent, setActiveContent] = useState("join");
-
-//   useEffect(() => {
-//     LayoutAnimation.easeInEaseOut();
-//   }, [activeContent]);
-
-//   // shows the content according to the button pressed
-//   const handleJoin = () => {
-//     setActiveContent("join");
-//   };
-//   const handleMyChallenges = () => {
-//     setActiveContent("my challenges");
-//   };
-
-//   return (
-//     <Screen style={styles.screen}>
-//       <TopNav showSearchBar={true} />
-//       {/* <Text style={styles.title}>group challenges</Text> */}
-//       <View style={styles.topBtnscontainer}>
-//         <TabButton
-//           text="Join"
-//           active={activeContent === "join"}
-//           onPress={handleJoin}
-//         />
-//         <TabButton
-//           text="My Challenges"
-//           active={activeContent === "my challenges"}
-//           onPress={handleMyChallenges}
-//         />
-//       </View>
-//       {/* content */}
-//       <View style={styles.contentContainer}>
-//         <FlatList
-//           data={data}
-//           keyExtractor={(item) => item.key}
-//           renderItem={({ item }) => {
-//             return (
-//               <View>
-//                 {item.key === "join" && activeContent === "join" && <Join />}
-//                 {item.key === "my challenges" &&
-//                   activeContent === "my challenges" && <MyChallenges />}
-//               </View>
-//             );
-//           }}
-//         />
-//       </View>
-//     </Screen>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   screen: {
-//     backgroundColor: colors.blackBc,
-//   },
-//   topBtnscontainer: {
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     marginTop: 20,
-//     // marginBottom: 20,
-//   },
-//   contentContainer: {
-//     flex: 1,
-//     marginTop: 10,
-//   },
-//   title: {
-//     fontFamily: "montserrat-black",
-//     fontSize: 18,
-//     color: colors.white,
-//     textTransform: "uppercase",
-//     alignSelf: "center",
-//     marginTop: 30,
-//   },
-// });
--- Front_End/app/screens/coach/CoachScreen.js
+++ Front_End/app/screens/coach/CoachScreen.js
@@ -1,167 +1,52 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  TouchableWithoutFeedback,
-  LayoutAnimation,
-  UIManager,
-  Platform,
-  Modal,
-} from "react-native";
-import React, { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import React from "react";
 
-import AppButtonBorder from "../../components/AppButtonBorder";
 import Screen from "../../components/Screen";
 import colors from "../../config/colors";
 import TopNav from "../../components/topNavigation/TopNav";
 import MeetTtrainer from "./coachScreenContent/MeetTtrainer";
 import MySessions from "./coachScreenContent/MySessions";
 
-// For android to enable Layoutanimation
-if (Platform.OS === "android") {
-  if (UIManager.setLayoutAnimationEnabledExperimental) {
-    UIManager.setLayoutAnimationEnabledExperimental(true);
-  }
-}
+const Tab = createMaterialTopTabNavigator();
 
-export default function CoachScreen({ navigation }) {
-  // to keep track of which button is active
-  const [activeContent, setActiveContent] = useState("meet trainers");
-  const [modalVisible, setModalVisible] = useState(false);
-
-  useEffect(() => {
-    LayoutAnimation.easeInEaseOut();
-  }, [activeContent]);
-
-  // shows the content according to the button pressed
-  const handleMeetTrainers = () => {
-    setActiveContent("meet trainers");
-  };
-  const handleMySessions = () => {
-    setActiveContent("my session");
-  };
-
+const CoachScreen = ({ navigation }) => {
   return (
     <Screen style={styles.screen}>
-      <View style={styles.nav}>
-        <TopNav
-          showSearchBar={true}
-          onPressMessage={() => console.log("message")}
-          onPressNotification={() =>
-            navigation.navigate("Notification", { screen: "coach" })
-          }
-        />
-      </View>
-      <ScrollView>
-        <View style={styles.topBtnscontainer}>
-          <TouchableWithoutFeedback
-            activeOpacity={0.9}
-            onPress={handleMeetTrainers}
-          >
-            <View
-              style={[
-                styles.btn,
-                activeContent === "meet trainers" && styles.activeBtn,
-              ]}
-            >
-              <Text
-                style={[
-                  styles.text,
-                  activeContent === "meet trainers" && styles.activeText,
-                ]}
-              >
-                meet trainers
-              </Text>
-            </View>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback
-            activeOpacity={0.9}
-            onPress={handleMySessions}
-          >
-            <View
-              style={[
-                styles.btn,
-                activeContent === "my session" && styles.activeBtn,
-              ]}
-            >
-              <Text
-                style={[
-                  styles.text,
-                  activeContent === "my session" && styles.activeText,
-                ]}
-              >
-                my session
-              </Text>
-            </View>
-          </TouchableWithoutFeedback>
-        </View>
-
-        <View style={styles.contentContainer}>
-          {activeContent === "meet trainers" ? (
-            <MeetTtrainer
-              onPressAllCoaches={() => navigation.navigate("CoachProfile")}
-              onPressClubCoaches={() => navigation.navigate("CoachProfile")}
-            />
-          ) : (
-            <MySessions />
-          )}
-        </View>
-      </ScrollView>
-      {activeContent === "meet trainers" && (
-        <View style={styles.fixButtonPosition}>
-          <AppButtonBorder
-            onPress={() => setModalVisible(true)}
-            title="Filter"
-            materialCommunityIcons={true}
-            iconName="filter-variant"
-          />
-        </View>
-      )}
+      <TopNav
+        showSearchBar={true}
+        onPressMessage={() => {
+          console.log("message");
+        }}
+        onPressNotification={() => {
+          navigation.navigate("Notification");
+        }}
+      />
+      <Tab.Navigator
+        // initialRouteName="Meet Trainers"
+        screenOptions={{
+          tabBarLabelStyle: {
+            fontFamily: "montserrat-black",
+            fontSize: 14,
+          },
+          tabBarIndicatorStyle: { backgroundColor: colors.green, height: 3 },
+          tabBarActiveTintColor: colors.green,
+          tabBarInactiveTintColor: colors.grayLight50,
+          tabBarStyle: { backgroundColor: colors.blackBc, height: 42 },
+        }}
+      >
+        <Tab.Screen name="Meet Trainers" component={MeetTtrainer} />
+        <Tab.Screen name="My Sessions" component={MySessions} />
+      </Tab.Navigator>
     </Screen>
   );
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
+    flex: 1,
     backgroundColor: colors.blackBc,
   },
-  fixButtonPosition: {
-    position: "absolute",
-    zIndex: 1,
-    right: 16,
-    marginBottom: 10,
-    bottom: 0,
-  },
-  topBtnscontainer: {
-    flexDirection: "row",
-    flex: 1,
-    justifyContent: "space-between",
-    marginTop: 20,
-    // marginBottom: 20,
-  },
-  btn: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-    borderBottomWidth: 2,
-    borderBottomColor: colors.grayLight50,
-  },
-  activeBtn: {
-    borderBottomColor: colors.green,
-  },
-  text: {
-    color: colors.grayLight50,
-    fontFamily: "montserrat-black",
-    fontSize: 14,
-    textTransform: "uppercase",
-  },
-  activeText: {
-    color: colors.green,
-  },
-  contentContainer: {
-    flex: 1,
-    marginTop: 10,
-  },
 });
+
+export default CoachScreen;
