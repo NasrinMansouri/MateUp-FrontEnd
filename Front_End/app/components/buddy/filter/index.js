ec6eeb6f44f99eba2e8231035104b1f5f22e776c
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-05-14 10:03:05 +0200

    channges filter folder placement

--- Front_End/app/components/buddy/filter/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Gender } from "./Gender";
-export { default as ModalHeader } from "./ModalHeader";
-export { default as DropDownList } from "./DropDownList";
-export { default as DatePicker } from "./DatePicker";
