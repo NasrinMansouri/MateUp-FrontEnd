6a343a379edb8bdb2aebc1c0a6a1b06fcd180d0f
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-05-14 10:10:17 +0200

    filter folder includes the requiremenet for use in filter modal for buddy and also for coach

---
+++ Front_End/app/components/filter/Gender.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableWithoutFeedback } from "react-native";
+
+import colors from "../../config/colors";
+
+export default function Gender() {
+  const [selected, setSelected] = useState(0); // 0: All, 1: Women, 2: Men
+
+  const handlePress = (index) => {
+    setSelected(index);
+  };
+
+  const boxStyles = (index) => {
+    return [
+      styles.box,
+      selected === index && styles.selectedBox,
+      selected === index && {
+        backgroundColor: colors.orangeSecondary,
+      },
+    ];
+  };
+
+  const textStyles = (index) => {
+    return [
+      styles.text,
+      selected === index && styles.selectedText,
+      selected === index && { color: colors.white },
+    ];
+  };
+
+  return (
+    <View style={styles.mainContainer}>
+      <Text style={styles.title}>Gender</Text>
+      <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={() => handlePress(0)}>
+          <View
+            style={[
+              boxStyles(0),
+              { borderTopLeftRadius: 4, borderBottomLeftRadius: 4 },
+            ]}
+          >
+            <Text style={textStyles(0)}>All</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => handlePress(1)}>
+          <View style={boxStyles(1)}>
+            <Text style={textStyles(1)}>Women</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => handlePress(2)}>
+          <View
+            style={[
+              boxStyles(2),
+              {
+                borderTopRightRadius: 4,
+                borderBottomRightRadius: 4,
+              },
+            ]}
+          >
+            <Text style={textStyles(2)}>Men</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginRight: 16,
+    marginLeft: 16,
+    marginBottom: 30,
+    marginTop: 100,
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 6,
+    backgroundColor: colors.grayLight50,
+    height: 53,
+    width: "100%",
+    borderRadius: 4,
+  },
+  title: {
+    color: colors.black,
+    fontFamily: "nunitoSans-bold",
+    fontSize: 16,
+    textTransform: "uppercase",
+  },
+  box: {
+    width: "33.3%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  selectedBox: {
+    backgroundColor: colors.orangeSecondary,
+  },
+  text: {
+    fontFamily: "nunitoSans-regular",
+    color: colors.black,
+    fontSize: 16,
+  },
+  selectedText: {
+    color: colors.white,
+  },
+});
---
+++ Front_End/app/components/filter/TimePicker.js
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Pressable,
+  TextInput,
+  Platform,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { FontAwesome } from "@expo/vector-icons";
+
+import colors from "../../config/colors";
+import AppButtonBorder from "../AppButtonBorder";
+
+export default function TimePicker({ title, setFieldValue, ...otherProps }) {
+  const [time, setTime] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+
+  const toggleTimePicker = () => {
+    setShowPicker(!showPicker);
+  };
+
+  const onChange = (event, selectedTime) => {
+    if (selectedTime) {
+      setTime(selectedTime);
+      if (Platform.OS === "android") {
+        toggleTimePicker();
+      }
+    }
+  };
+
+  const confirmIOSTime = () => {
+    toggleTimePicker();
+  };
+
+  const cancelIOSTime = () => {
+    setTime(new Date()); // Reset the time to the current time
+    toggleTimePicker();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      {showPicker && Platform.OS === "ios" && (
+        <View style={styles.btnContainer}>
+          <AppButtonBorder
+            onPress={cancelIOSTime}
+            title="Cancel"
+            height={40}
+            width={100}
+            backgroundColor={colors.white}
+            borderColor={colors.black}
+            textColor={colors.black}
+          />
+          <AppButtonBorder
+            onPress={confirmIOSTime}
+            title="Confirm"
+            height={40}
+            width={100}
+            backgroundColor={colors.black}
+            borderColor={colors.green}
+            textColor={colors.orangePrimary}
+          />
+        </View>
+      )}
+      {!showPicker && (
+        <Pressable onPress={toggleTimePicker}>
+          <View style={styles.textInputContainer}>
+            <TextInput
+              placeholder="Select Time"
+              value={
+                time
+                  ? time.toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })
+                  : ""
+              }
+              editable={false}
+              style={styles.textInput}
+              placeholderTextColor={colors.gray}
+              {...otherProps}
+            />
+            <FontAwesome
+              name="chevron-down"
+              size={12}
+              color={colors.green}
+              style={styles.icon}
+            />
+          </View>
+        </Pressable>
+      )}
+
+      {showPicker && (
+        <DateTimePicker
+          mode="time"
+          display="spinner"
+          value={time}
+          onChange={onChange}
+          style={styles.datePicker}
+          textColor={colors.black}
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginRight: 16,
+    marginLeft: 16,
+    marginBottom: 50,
+  },
+  textInputContainer: {
+    position: "relative",
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 5,
+    marginTop: 20,
+    marginBottom: 30,
+    marginLeft: 16,
+    marginRight: 16,
+  },
+  title: {
+    fontFamily: "nunitoSans-bold",
+    fontSize: 16,
+    textTransform: "uppercase",
+    marginBottom: 6,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: colors.grayLight50,
+    borderRadius: 4,
+    flex: 1,
+    width: "100%",
+    height: 45,
+    paddingRight: 30,
+    paddingLeft: 20,
+  },
+  icon: {
+    position: "absolute",
+    right: 20,
+    top: 15,
+  },
+});
