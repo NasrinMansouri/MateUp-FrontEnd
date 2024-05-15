cc0fff892a40e156990d4a91a047e90e95842bfe
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-05-15 20:43:38 +0200

    commented content for session screen in coach section

--- Front_End/app/screens/coach/coachScreenContent/MySessions.js
+++ Front_End/app/screens/coach/coachScreenContent/MySessions.js
@@ -34,53 +34,52 @@ const styles = StyleSheet.create({
 });
 
 const sessionDetailsData = [
-  {
-    id: 1,
-    trainerName: "Jen Smith",
-    imageTrainer: require("../../../../assets/person3.jpg"),
-    sessionGoal: "Get Strong and get toned",
-    date: "Aug 3",
-    start: "5 PM",
-    end: "7 PM",
-    members: [
-      {
-        id: 1,
-        image: require("../../../../assets/person3.jpg"),
-      },
-      {
-        id: 2,
-        image: require("../../../../assets/person3.jpg"),
-      },
-      {
-        id: 3,
-        image: require("../../../../assets/person3.jpg"),
-      },
-      {
-        id: 4,
-        image: require("../../../../assets/person3.jpg"),
-      },
-      {
-        id: 5,
-        image: require("../../../../assets/person3.jpg"),
-      },
-    ],
-  },
-
-  {
-    id: 2,
-    trainerName: "Jen Smith",
-    imageTrainer: require("../../../../assets/person3.jpg"),
-    sessionGoal: "Get Strong and get toned",
-    date: "Aug 3",
-    start: "5 PM",
-    end: "7 PM",
-    members: [
-      {
-        id: 1,
-        image: require("../../../../assets/person3.jpg"),
-      },
-    ],
-  },
+  // {
+  //   id: 1,
+  //   trainerName: "Jen Smith",
+  //   imageTrainer: require("../../../../assets/person3.jpg"),
+  //   sessionGoal: "Get Strong and get toned",
+  //   date: "Aug 3",
+  //   start: "5 PM",
+  //   end: "7 PM",
+  //   members: [
+  //     {
+  //       id: 1,
+  //       image: require("../../../../assets/person3.jpg"),
+  //     },
+  //     {
+  //       id: 2,
+  //       image: require("../../../../assets/person3.jpg"),
+  //     },
+  //     {
+  //       id: 3,
+  //       image: require("../../../../assets/person3.jpg"),
+  //     },
+  //     {
+  //       id: 4,
+  //       image: require("../../../../assets/person3.jpg"),
+  //     },
+  //     {
+  //       id: 5,
+  //       image: require("../../../../assets/person3.jpg"),
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 2,
+  //   trainerName: "Jen Smith",
+  //   imageTrainer: require("../../../../assets/person3.jpg"),
+  //   sessionGoal: "Get Strong and get toned",
+  //   date: "Aug 3",
+  //   start: "5 PM",
+  //   end: "7 PM",
+  //   members: [
+  //     {
+  //       id: 1,
+  //       image: require("../../../../assets/person3.jpg"),
+  //     },
+  //   ],
+  // },
 ];
 
 {
