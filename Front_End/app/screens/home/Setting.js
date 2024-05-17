c313394b2a60c02f18959910c2f41e8001c7f33f
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-05-17 12:42:18 +0200

    creates screens for setting and profile, is empty for now

---
+++ Front_End/app/screens/home/Setting.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Setting() {
+  return (
+    <View style={styles.container}>
+      <Text>Setting</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
---
+++ Front_End/app/screens/home/UserProfileScreen.js
@@ -0,0 +1,19 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+import Screen from "../../components/Screen";
+
+export default function UserProfileScreen() {
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <Text style={{ color: "white" }}>User Profile</Text>
+      </View>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
