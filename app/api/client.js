304d9687adccdbedf5a70c15d39fcf1cccea292b
MateaKarst <matea.karst@gmail.com>
2024-05-21 13:15:08 +0200

    connecting with backend api works

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,14 @@
 import { create } from "apisauce";
+import { Platform } from "react-native";
 
+let url;
+if (Platform.OS == 'android') {
+  url = "http://10.0.2.2:8000/api";
+}
+else {
+  url = "http://127.0.0.1:8000/api";
+}
 const apiClient = create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: url
 });
-
 export default apiClient;
