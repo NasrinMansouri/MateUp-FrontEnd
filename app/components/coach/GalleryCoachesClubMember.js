a704fcb9ab9d77904e442c1d37fcac49dbcaba51
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-05-23 11:58:10 +0200

    files that need to be deleted, dublicated file!

--- app/components/coach/GalleryCoachesClubMember.js
+++ app/components/coach/GalleryCoachesClubMember.js
@@ -1,52 +1,52 @@
-// also can be use for meet  at user members
+// // also can be use for meet  at user members
 
-import { StyleSheet, Text, View, FlatList } from "react-native";
-import React from "react";
-import { useNavigation } from "@react-navigation/native";
+// import { StyleSheet, Text, View, FlatList } from "react-native";
+// import React from "react";
+// import { useNavigation } from "@react-navigation/native";
 
-import colors from "../../config/colors";
-import CardCoachClubMember from "./CardCoachClubMember";
+// import colors from "../../config/colors";
+// import CardCoachClubMember from "./CardCoachClubMember";
 
-export default function GalleryCoachesClubMembers({
-  coachesClubMember,
-  onPressClubCoaches,
-}) {
-  const navigation = useNavigation();
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Coaches at your club</Text>
-      <FlatList
-        style={styles.container}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        data={coachesClubMember}
-        keyExtractor={(coachesClubMember) => coachesClubMember.id.toString()}
-        renderItem={({ item }) => (
-          <CardCoachClubMember
-            name={item.name}
-            image={item.image}
-            titles={item.titles}
-            onPress={() => navigation.navigate("CoachProfile", item)}
-          />
-        )}
-      />
-    </View>
-  );
-}
+// export default function GalleryCoachesClubMembers({
+//   coachesClubMember,
+//   onPressClubCoaches,
+// }) {
+//   const navigation = useNavigation();
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Coaches at your club</Text>
+//       <FlatList
+//         style={styles.container}
+//         horizontal
+//         showsHorizontalScrollIndicator={false}
+//         data={coachesClubMember}
+//         keyExtractor={(coachesClubMember) => coachesClubMember.id.toString()}
+//         renderItem={({ item }) => (
+//           <CardCoachClubMember
+//             name={item.name}
+//             image={item.image}
+//             titles={item.titles}
+//             onPress={() => navigation.navigate("CoachProfile", item)}
+//           />
+//         )}
+//       />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 40,
-  },
-  title: {
-    fontFamily: "montserrat-black",
-    fontSize: 36,
-    color: colors.orangePrimary,
-    marginBottom: 10,
-    textTransform: "uppercase",
-    paddingLeft: 16,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     paddingTop: 40,
+//   },
+//   title: {
+//     fontFamily: "montserrat-black",
+//     fontSize: 36,
+//     color: colors.orangePrimary,
+//     marginBottom: 10,
+//     textTransform: "uppercase",
+//     paddingLeft: 16,
+//   },
+// });
 
 //to be use in screen as:
 {
