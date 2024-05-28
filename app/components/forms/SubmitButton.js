0c13aaf03e1cbd9ad608235cd050488745f14955
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-05-28 08:08:10 +0200

    chnges color of submit btn

--- app/components/forms/SubmitButton.js
+++ app/components/forms/SubmitButton.js
@@ -3,11 +3,19 @@ import React from "react";
 import { useFormikContext } from "formik";
 
 import AppButton from "../AppButton";
+import colors from "../../config/colors";
 
 export default function SubmitButton({ title }) {
   const { handleSubmit } = useFormikContext();
 
-  return <AppButton title={title} height={48} onPress={handleSubmit} />;
+  return (
+    <AppButton
+      title={title}
+      height={48}
+      onPress={handleSubmit}
+      backgroundColor={colors.orangeSecondary}
+    />
+  );
 }
 
 const styles = StyleSheet.create({});
