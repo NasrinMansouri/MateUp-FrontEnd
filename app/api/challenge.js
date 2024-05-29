cf770b4e05091ddf7d7402d131eb468315256149
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-05-29 10:35:30 +0200

    adds code to challneg api file, still working on posting data to backend. also, now adds a progress bar to it

--- app/api/challenge.js
+++ app/api/challenge.js
@@ -1,6 +1,6 @@
 import client from "./client";
 
-const createChallenge = () => (challenge) => {
+const createChallenge = (challenge, onUploadProgress) => {
   // content-type when we send json object to the server
   // content type is automatically set to application/json
   // multipart/form-data for when we send files-img to the server
@@ -12,13 +12,21 @@ const createChallenge = () => (challenge) => {
   });
   data.append("workout", challenge.workout);
   data.append("base", challenge.base);
-  data.append("goal", challenge.goal);
+  // data.append("goalFrequency", challenge.goalFrequency);
+  // data.append("goalTime", challenge.goalTime);
+  if (challenge.goalFrequency)
+    data.append("goalFrequency", JSON.stringify(challenge.goalFrequency));
+  if (challenge.goalTime) data.append("goalTime", challenge.goalTime);
   data.append("start", challenge.start);
   data.append("end", challenge.end);
   data.append("name", challenge.name);
   data.append("description", challenge.description);
 
-  return client.post("/myChallenges", data);
+  return client.post("/myChallenges", data, {
+    onUploadProgress: (progress) => {
+      onUploadProgress(progress.loaded / progress.total);
+    },
+  });
 };
 
 export default { createChallenge };
