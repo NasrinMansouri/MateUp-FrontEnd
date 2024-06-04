f273c316fe4669a9390d1349bcf3cd7fa8eec120
Nasrin Mansouri <nasrin.mansouri477@gmail.com>
2024-06-04 14:05:29 +0200

    builds auth folder, and add it to app.js for login

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { navigationRef } from "./app/navigation/rootNavigation";
+import { AppLoading } from "expo";
 
 // to change the background color
 import NavigationTheme from "./app/navigation/NavigationTheme";
@@ -14,6 +15,8 @@ import { useCallback } from "react";
 SplashScreen.preventAutoHideAsync();
 
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import AuthContext from "./app/auth/context";
+import authStorage from "./app/auth/storage";
 import AppNavigator from "./app/navigation/AppNavigator";
 import LoginScreen from "./app/screens/LoginScreen";
 import myTheme from "./app/navigation/NavigationTheme";
@@ -446,6 +449,20 @@ const memberProfile = {
 };
 
 export default function App() {
+  // for auth
+  const [user, setUser] = useState();
+  const [isready, setIsReady] = useState(false);
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
+  };
+
+  if (!isReady)
+    return (
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
+    );
+
   //for fonts
   const [isLoaded] = useFonts({
     "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
@@ -464,12 +481,15 @@ export default function App() {
 
   return (
     <>
-      <SafeAreaProvider onLayout={handleOnLayout}>
-        <NavigationContainer ref={navigationRef} theme={myTheme}>
-          <AppNavigator />
-          {/* <AuthNavigator /> */}
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <AuthContext.Provider value={{ user, setUser }}>
+        <SafeAreaProvider onLayout={handleOnLayout}>
+          <NavigationContainer ref={navigationRef} theme={myTheme}>
+            {/* <AppNavigator /> */}
+            {/* <AuthNavigator /> */}
+            {user ? <AppNavigator /> : <AuthNavigator />}
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </AuthContext.Provider>
     </>
   );
 }
